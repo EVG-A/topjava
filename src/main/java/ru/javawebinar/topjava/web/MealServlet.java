39197c7245d7eef3c374b3ab3d795e30109315f5
EVG-A <jenya.86@mail.ru>
2020-02-09 19:21:06 +0300

    HW1.1

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,49 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class MealServlet extends HttpServlet {
+
+    private static final Logger log = getLogger(MealServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("redirect to meals");
+        request.setAttribute("meals", getMeals());
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+    }
+
+    public static List<MealTo> getMeals() {
+
+        List<Meal> meals = Arrays.asList(
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 400),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+        );
+        Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
+                .collect(Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories)));
+        return meals.stream()
+                .map(meal -> new MealTo(meal.getDateTime(), meal.getDescription(), meal.getCalories(), caloriesSumByDate.get(meal.getDate()) > 2000))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,30 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<html>
+<head>
+    <title>Meals</title>
+</head>
+<body>
+<h3><a href="index.html">Home</a></h3>
+<hr>
+<h2>Meals</h2>
+<table>
+    <tr>
+        <th>Дата/Время</th>
+        <th>Описание</th>
+        <th>Калории</th>
+    </tr>
+    <c:forEach var="mealTo" items="${requestScope.meals}">
+        <tr style="color:${mealTo.excess ? 'green' : 'red'}">
+            <fmt:parseDate value="${mealTo.dateTime}" var="parsedEmpDate" pattern="yyyy-MM-dd'T'HH:mm"/>
+            <fmt:formatDate pattern="yyyy-MM-dd HH:mm" value="${ parsedEmpDate }" var="resultDate"/>
+            <th><c:out value="${resultDate}"/></th>
+            <th>${mealTo.description}</th>
+            <th>${mealTo.calories}</th>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
